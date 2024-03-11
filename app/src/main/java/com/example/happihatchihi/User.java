9e78f289ff7621a2805a14b8154a492b1af92f18
FMossman <fmcmossman@gmail.com>
2024-03-11 13:53:48 +0000

    first commit

---
+++ app/src/main/java/com/example/happihatchihi/User.java
@@ -0,0 +1,153 @@
+package com.example.happihatchihi;
+
+
+/**
+ * The User class represents a user in the system.
+ */
+public class User {
+
+    private String username;
+    private String password;
+    private String email;
+    private int age;
+    private String gender;
+    private String firstName;
+    private String lastName;
+
+    public User(String username, String password, String email, int age, String gender, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.age = age;
+        this.gender = gender;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * Returns the first name of the user.
+     *
+     * @return the first name of the user
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets the first name of the user.
+     * @param firstName the new first name of the user
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Returns the last name of the user.
+     *
+     * @return the last name of the user
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets the last name of the user.
+     *
+     * @param lastName the new last name of the user
+     */
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the username of the user
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets the username of the user.
+     *
+     * @param username the new username of the user
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Returns the password of the user.
+     *
+     * @return the password of the user
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets the password of the user.
+     *
+     * @param password the new password of the user
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Returns the email of the user.
+     *
+     * @return the email of the user
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the email of the user.
+     *
+     * @param email the new email of the user
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Returns the age of the user.
+     *
+     * @return the age of the user
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Sets the age of the user.
+     *
+     * @param age the new age of the user
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    /**
+     * Returns the gender of the user.
+     *
+     * @return the gender of the user
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * Sets the gender of the user.
+     *
+     * @param gender the new gender of the user
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
+
