115a8dd812c770447ea3e2bacee3201480602417
RJW19 <148894701+RJW19@users.noreply.github.com>
2024-03-18 09:05:04 +0000

    refactored the code to use hashmap instead of arraylist for the usermanager class.

--- app/src/main/java/com/example/happihatchihi/UserManager.java
+++ app/src/main/java/com/example/happihatchihi/UserManager.java
@@ -1,4 +1,6 @@
-import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The UserManager class manages a collection of User objects.
@@ -6,84 +8,69 @@ import java.util.ArrayList;
  */
 public class UserManager {
 
-    private ArrayList<User> users;
+    private Map<String, User> users;
 
     /**
      * Constructs a new UserManager object with an empty list of users.
      */
     public UserManager() {
-        users = new ArrayList<User>();
+        users = new HashMap<>();
     }
 
     /**
-     * Adds a user to the list of users.
+     * Adds a user to the collection of users.
      * @param user The user to be added.
      */
     public void addUser(User user) {
-        users.add(user);
+        users.put(user.getUsername(), user);
     }
 
     /**
-     * Removes a user from the list of users.
+     * Removes a user from the collection of users.
      * @param user The user to be removed.
      */
     public void removeUser(User user) {
-        users.remove(user);
+        users.remove(user.getUsername());
     }
 
     /**
-     * Retrieves a user with the specified username.
-     * @param username The username of the user to retrieve.
-     * @return The user with the specified username, or null if no such user exists.
+     * Retrieves a user from the collection of users based on the username.
+     * @param username The username of the user to be retrieved.
+     * @return The User object associated with the given username, or null if no such user exists.
      */
     public User getUser(String username) {
-        for (User user : users) {
-            if (user.getUsername().equals(username)) {
-                return user;
-            }
-        }
-        return null;
+        return users.get(username);
     }
 
     /**
-     * Checks if a user with the specified username exists.
-     * @param username The username to check.
-     * @return true if a user with the specified username exists, false otherwise.
+     * Checks if a user with the given username exists in the collection of users.
+     * @param username The username to be checked.
+     * @return true if a user with the given username exists, false otherwise.
      */
     public boolean userExists(String username) {
-        for (User user : users) {
-            if (user.getUsername().equals(username)) {
-                return true;
-            }
-        }
-        return false;
+        return users.containsKey(username);
     }
 
     /**
-     * Checks if a password matches the given username.
-     * @param username The username to check.
-     * @param password The password to check.
-     * @return true if the password matches the given username, false otherwise.
+     * Checks if the given password matches the password associated with the given username.
+     * @param username The username to be checked.
+     * @param password The password to be checked.
+     * @return true if the password matches, false otherwise.
      */
     public boolean passwordMatches(String username, String password) {
-        for (User user : users) {
-            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                return true;
-            }
-        }
-        return false;
+        User user = users.get(username);
+        return user != null && user.getPassword().equals(password);
     }
 
     /**
-     * Creates a new user with the provided username and password.
+     * Creates a new user with the given username and password, and adds it to the collection of users.
      * @param username The username of the new user.
      * @param password The password of the new user.
      * @return The newly created User object.
      */
     public User createUser(String username, String password) {
         User newUser = new User(username, password);
-        users.add(newUser);
+        users.put(username, newUser);
         return newUser;
     }
 }
-
