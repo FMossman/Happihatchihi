45be32d4ee3c039c8300111ae7c3a240f35b6c29
RJW19 <148894701+RJW19@users.noreply.github.com>
2024-03-22 01:41:42 +0000

    Created SetInitialGoalsFragment java and xml. added radio buttons for these. Created Initial goal details page with spinners for freq, recurrence and notifications. Added wrappers for icons to allow for use inside radio buttons
    ToDo:
    Add notification timers?
    Change header based on radio button selection, at the moment this is hard coded.
    Add summary page for goals

--- app/src/main/java/com/example/happihatchihi/frontend/SetInitialGoalsFragment.java
+++ app/src/main/java/com/example/happihatchihi/frontend/SetInitialGoalsFragment.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.happihatchihi.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -42,23 +47,19 @@ public class SetInitialGoalsFragment extends Fragment {
 
     }
 
-    /**
-     * Called when the view for this fragment is created.
-     * Inflates the layout for this fragment and sets up the click listeners for the SetInitialGoals button,
-     * register text view, and forgot password text view.
-     *
-     * @param inflater           The LayoutInflater object that can be used to inflate any views in the fragment.
-     * @param container          The parent view that the fragment's UI should be attached to.
-     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state.
-     * @return The View for the fragment's UI, or null.
-     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_setinitialgoals, container, false);
 
+        Button nextButton = view.findViewById(R.id.initialGoalsNextButton);
 
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getParentFragmentManager().beginTransaction().replace(R.id.fragment_container, new SetInitialGoalDetailsFragment()).commit();
+            }
+        });
         return view;
     }
 }
