8dbe131a6f12647c96000b2976fe063521631b49
DeborahAyebare <161496591+DeborahAyebare@users.noreply.github.com>
2024-03-23 00:30:18 +0000

    Edit notifications fragment.

---
+++ app/src/main/java/com/example/happihatchihi/frontend/EditNotificationsFragment.java
@@ -0,0 +1,24 @@
+package com.example.happihatchihi.frontend;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.happihatchihi.R;
+
+public class EditNotificationsFragment extends Fragment {
+
+    public EditNotificationsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_notifications, container, false);
+    }
+}
