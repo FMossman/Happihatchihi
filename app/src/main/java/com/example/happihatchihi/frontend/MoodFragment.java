357dd18f8df57ccef0faa7dbea505f008f1fa304
RJW19 <148894701+RJW19@users.noreply.github.com>
2024-03-24 22:52:56 +0000

    Shuffled ordering of elif in register screen to allow for password confirmation request pop-up, added pop-up on other missing infos also.

--- app/src/main/java/com/example/happihatchihi/frontend/MoodFragment.java
+++ app/src/main/java/com/example/happihatchihi/frontend/MoodFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +26,8 @@ public class MoodFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.mood_pop_up, container, false);
+        View view = inflater.inflate(R.layout.mood_pop_up, container, false);
+
+        return view;
     }
 }
