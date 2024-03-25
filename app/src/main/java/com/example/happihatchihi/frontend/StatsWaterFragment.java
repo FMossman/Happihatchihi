76fa7b0612f70726517dbeac59347a85c48cca46
FMossman <fmcmossman@gmail.com>
2024-03-25 01:42:29 +0000

    Added the stats page, still need to add the goal history in the morning.

---
+++ app/src/main/java/com/example/happihatchihi/frontend/StatsWaterFragment.java
@@ -0,0 +1,40 @@
+package com.example.happihatchihi.frontend;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.happihatchihi.R;
+
+/**
+ * A fragment for holding information about a goal's history.
+ */
+public class StatsWaterFragment extends Fragment {
+
+
+
+
+    public StatsWaterFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stats_water, container, false);
+    }
+}
